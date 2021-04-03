22f0536c6fa72201c3ecd4bc8ae630a2cca42942
Hinal Pujara <hinal.pujara@somaiya.edu>
2021-04-03 23:35:03 +0530

    Complecated php file

--- University-Website-hackathon/Index/order-form.php
+++ University-Website-hackathon/Index/order-form.php
@@ -7,12 +7,38 @@
   <title>Order-Form</title>
   <meta name="description" content="The HTML5 Herald">
   <meta name="author" content="SitePoint">
+  <style>
+    
+  </style>
 
   <link rel="stylesheet" href="order-form-style.css">
-
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 </head>
 
 <body>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color:rgb(12, 12, 12)">
+    <a class="navbar-brand" href="#">
+        <img src="logo4.jpg" class="d-inline-block align-center" alt="">
+    </a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active pl-3">
+                <a class="nav-link" href="index.html" style="color: white" ;><b>Home</b> <span
+                        class="sr-only"></span></a>
+            </li>
+            <li class="nav-item pl-3">
+                <a class="nav-link" href="canteen.html" style="color: white;"><b>Canteen service</b> </span></a>
+            </li>
+            <li class="nav-item pl-3">
+                <a class="nav-link" href="lost1.html" style="color: white;"><b>Lost and found service</b></a>
+            </li>
+        </ul>
+        
+    </div>
+</nav>
+<br>
+<br>
+
     <div class="container">
         <form action="submit_order.php" method="post">
           <div class="row">
@@ -58,7 +84,7 @@
               <textarea id="subject" name='subject' placeholder="Write something.." style="height:200px"></textarea>
             </div>
           </div>
-          <div class="row">
+          <div class="row" >
          <input type="submit" value="submit">
            </div>
         </form>
